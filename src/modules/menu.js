77aac037c679170dc3d67996f77d21554ab1a561
stokato <demyanovpaul@yandex.ru>
2024-01-11 02:33:43 +0300

    lesson20 - усложненные тоже сделаны

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -4,14 +4,36 @@ const menu = () => {
   const menu = document.querySelector('menu');
   const closeBtn = menu.querySelector('.close-btn');
   const menuItems = menu.querySelectorAll('ul>li>a');
+  const serviceBlock = document.querySelector('a[href="#service-block"]');
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
   };
 
+  const animaitionScrool = (a) => {
+    const element = document.querySelector(a.getAttribute('href'));
+    if (element && document.body.clientWidth > 767) {
+      window.scrollTo({
+        top: Math.floor(element.getBoundingClientRect().top),
+        behavior: 'smooth',
+      });
+    }
+  };
+
   menuBtn.addEventListener('click', handleMenu);
   closeBtn.addEventListener('click', handleMenu);
-  menuItems.forEach((item) => item.addEventListener('click', handleMenu));
+  serviceBlock.addEventListener('click', (event) => {
+    event.preventDefault();
+    animaitionScrool(event.target.parentElement);
+  });
+
+  menuItems.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      event.preventDefault();
+      animaitionScrool(event.target);
+      handleMenu();
+    });
+  });
 };
 
 export default menu;
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,19 +4,22 @@ const modal = () => {
   const buttons = document.querySelectorAll('.popup-btn');
   const closeBtn = document.querySelector('.popup-close');
   let opacity = 0;
-  const animation = () => {
-    debugger;
+  const animationOpacity = () => {
     if (modal.style.opacity < 101) {
-      modal.style.opacity = opacity;
-      opacity += 1;
+      modal.style.opacity = `${opacity}%`;
+      opacity += 5;
     }
   };
   buttons.forEach((button) => button.addEventListener('click', () => {
-    requestAnimationFrame(animation);
     modal.style.display = 'block';
+    if (document.body.clientWidth > 767) {
+      opacity = 0;
+      modal.style.opacity = '0%';
+      setInterval(() => requestAnimationFrame(animationOpacity), 25);
+    }
   }));
   closeBtn.addEventListener('click', () => {
-    cancelAnimationFrame(animation);
+    cancelAnimationFrame(animationOpacity);
     modal.style.display = '';
   });
 };
