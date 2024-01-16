b65d78b3736e0fbf12c4871de47e720a274833a1
stokato <demyanovpaul@yandex.ru>
2024-01-16 22:34:28 +0300

    lesson23 - усложненное вопрос

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal';
 import regular from './modules/regular';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import sliderCarousel from './modules/sliderCarousel';
 
 timerModule('13 january 2024 01:01:00');
 menu();
@@ -11,3 +12,4 @@ modal();
 regular();
 tabs();
 slider();
+sliderCarousel();
